bbc320dc5e1103a8c06ca16580ee44c09ebb5976
Kyle Buchanan <kylebuch8@gmail.com>
2022-10-10 14:01:38 -0400

    updating the api response

--- pages/api/data.js
+++ pages/api/data.js
@@ -1,5 +1,7 @@
 import * as urgentCareData from "./urgent-care-data.json";
 
 export default function handler(req, res) {
-    res.status(200).json(urgentCareData.features.slice(0, 200));
+    const data = Object.assign({}, urgentCareData);
+    const locations = data.features.slice(0, 200);
+    res.status(200).json(locations);
 }
