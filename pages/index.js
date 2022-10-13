97d952bd7571fc54f48e06477d375e92005d603a
Kyle Buchanan <kylebuch8@gmail.com>
2022-10-13 11:31:47 -0400

    removing icon from the index page

--- pages/index.js
+++ pages/index.js
@@ -14,12 +14,11 @@ export default function Index() {
       </Head>
 
       <Script type="module" src="https://unpkg.com/@patternfly/pfe-cta@1.12.3/dist/pfe-cta.min.js" />
-      <Script type="module" src="https://unpkg.com/@patternfly/pfe-icon@1.12.3/dist/pfe-icon.min.js" />
 
       <h1 className={styles.slideUp}>NC Urgent Care Finder</h1>
       <p className={styles.slideUp} style={{"--slideUp-delay": "0.1s"}}>Fast, convenient and affordable care near you.</p>
       <div className={styles.slideUp} style={{"--slideUp-delay": "0.2s"}}>
-      <pfe-cta priority="primary"><Link href="/home" passHref><a><pfe-icon icon="rh-icon-handshake"></pfe-icon>Find a location</a></Link></pfe-cta>
+      <pfe-cta priority="primary"><Link href="/home" passHref><a>Find a location</a></Link></pfe-cta>
       </div>
     </div>
   )
