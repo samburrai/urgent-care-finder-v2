c9a32ce6f53bec3b915991bb4fe95ffd7bce8e2b
Kyle Buchanan <kylebuch8@gmail.com>
2022-10-17 17:13:20 -0400

    footer updates

--- components/footer.js
+++ components/footer.js
@@ -3,7 +3,12 @@ import styles from "../styles/Footer.module.css"
 export default function Footer() {
   return (
     <footer className={styles.footer}>
-      Footer
+      <ul>
+        <li>Deployed with <a href="https://vercel.com">Vercel</a></li>
+        <li>
+          <a href="https://github.com/kylebuch8/urgent-care-finder-v2/">View the code on GitHub</a>
+        </li>
+      </ul>
     </footer>
   )
 }
